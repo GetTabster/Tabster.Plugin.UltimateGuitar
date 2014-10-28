25e96e89e9b1cd64a03976741c024bcfa1e0bfa8
Nate Shoffner <nate.shoffner@gmail.com>
2014-10-28 16:17:15 -0400

    Missed in previous commit.

---
+++ UltimateGuitarParser.cs
@@ -0,0 +1,117 @@
+﻿#region
+
+using System;
+using System.Text.RegularExpressions;
+using HtmlAgilityPack;
+using Tabster.Core.Types;
+using Tabster.Data;
+using Tabster.Data.Processing;
+
+#endregion
+
+namespace UltimateGuitar
+{
+    internal class UltimateGuitarParser : ITablatureWebpageImporter
+    {
+        #region Implementation of ITabParser
+
+        public string SiteName
+        {
+            get { return "Ultimate Guitar"; }
+        }
+
+        public TablatureDocument Parse(string text, TablatureType type)
+        {
+            var doc = new HtmlDocument();
+            doc.LoadHtml(text);
+
+            string song = null, artist = null;
+
+            //get values from meta keywords
+            var metaNodes = doc.DocumentNode.SelectNodes("/html/head/meta");
+            foreach (var mn in metaNodes)
+            {
+                if (mn.HasAttributes && mn.Attributes.Contains("name") && mn.Attributes["name"].Value == "keywords")
+                {
+                    var split = mn.Attributes["content"].Value.Split(',');
+                    song = split[0].Trim();
+
+                    var typeStr = split[1].Trim();
+
+                    if (type == null)
+                    {
+                        if (typeStr.IndexOf("bass", StringComparison.OrdinalIgnoreCase) > -1)
+                            type = TablatureType.Bass;
+                        else if (typeStr.IndexOf("chord", StringComparison.OrdinalIgnoreCase) > -1)
+                            type = TablatureType.Chords;
+                        else if (typeStr.IndexOf("drum", StringComparison.OrdinalIgnoreCase) > -1)
+                            type = TablatureType.Drum;
+                        else if (typeStr.IndexOf("ukulele", StringComparison.OrdinalIgnoreCase) > -1)
+                            type = TablatureType.Ukulele;
+                    }
+
+                    artist = split[2].Trim();
+                    break;
+                }
+            }
+
+            var contentsNode = doc.DocumentNode.SelectSingleNode("//div[@id='cont']/pre[2]");
+
+            if (contentsNode != null)
+            {
+                var contents = StripHTML(contentsNode.InnerHtml);
+                contents = ConvertNewlines(contents);
+                return new TablatureDocument(artist, song, type, contents);
+            }
+
+            return null;
+        }
+
+        public bool MatchesUrlPattern(Uri url)
+        {
+            return url.IsWellFormedOriginalString() && ((url.DnsSafeHost == "ultimate-guitar.com" || url.DnsSafeHost == "www.ultimate-guitar.com" ||
+                                                         url.DnsSafeHost == "tabs.ultimate-guitar.com") && url.AbsolutePath.Split('/').Length >= 4);
+        }
+
+        #endregion
+
+        private static readonly Regex NewlineRegex = new Regex("(?<!\r)\n", RegexOptions.Compiled);
+
+        private static string ConvertNewlines(string source)
+        {
+            return NewlineRegex.Replace(source, Environment.NewLine);
+        }
+
+        private static string StripHTML(string source)
+        {
+            var array = new char[source.Length];
+            var arrayIndex = 0;
+            var inside = false;
+
+            for (var i = 0; i < source.Length; i++)
+            {
+                var let = source[i];
+
+                if (let == '<')
+                {
+                    inside = true;
+                    continue;
+                }
+
+                if (let == '>')
+                {
+                    inside = false;
+                    continue;
+                }
+
+                if (!inside)
+                {
+                    array[arrayIndex] = let;
+                    arrayIndex++;
+                }
+            }
+
+            return new string(array, 0, arrayIndex);
+        }
+    }
+}
---
+++ UltimateGuitarSearch.cs
@@ -0,0 +1,220 @@
+﻿#region
+
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Web;
+using HtmlAgilityPack;
+using Tabster.Core.Searching;
+using Tabster.Core.Types;
+using Tabster.Data.Processing;
+
+#endregion
+
+namespace UltimateGuitar
+{
+    public class UltimateGuitarSearch : ISearchService
+    {
+        #region Implementation of ISearchService
+
+        public ITablatureWebpageImporter Parser
+        {
+            get { return new UltimateGuitarParser(); }
+        }
+
+        public string Name
+        {
+            get { return "Ultimate Guitar"; }
+        }
+
+        public SearchServiceFlags Flags
+        {
+            get { return SearchServiceFlags.None; }
+        }
+
+        public WebProxy Proxy { get; set; }
+
+        public bool SupportsRatings
+        {
+            get { return true; }
+        }
+
+        public SearchResult[] Search(SearchQuery query)
+        {
+            var results = new List<SearchResult>();
+
+            var urlEncodedQuery = HttpUtility.UrlEncode(string.Format("{0} {1}", query.Artist, query.Title));
+
+            string typeStr = null;
+
+            if (query.Type != null)
+            {
+                if (query.Type == TablatureType.Guitar)
+                {
+                    typeStr = "200";
+                }
+                else if (query.Type == TablatureType.Chords)
+                {
+                    typeStr = "300";
+                }
+                else if (query.Type == TablatureType.Bass)
+                {
+                    typeStr = "400";
+                }
+                else if (query.Type == TablatureType.Drum)
+                {
+                    typeStr = "700";
+                }
+                else if (query.Type == TablatureType.Ukulele)
+                {
+                    typeStr = "800";
+                }
+            }
+
+            var urlBuilder = new StringBuilder();
+            urlBuilder.AppendFormat("http://www.ultimate-guitar.com/search.php?w=songs&s={0}", urlEncodedQuery);
+
+            if (!string.IsNullOrEmpty(typeStr))
+                urlBuilder.AppendFormat("&type={0}", typeStr);
+
+            var url = new Uri(urlBuilder.ToString());
+
+            string data;
+
+            var client = new WebClient {Proxy = Proxy};
+            {
+                data = client.DownloadString(url);
+            }
+
+            if (data.Length > 0 && !data.Contains("No results"))
+            {
+                var doc = new HtmlDocument();
+                doc.LoadHtml(data);
+
+                var tresults = doc.DocumentNode.SelectSingleNode("//table[@class='tresults']");
+
+                if (tresults != null)
+                {
+                    var count = 0;
+                    var rows = tresults.SelectNodes("tr");
+                    var loopArtist = ""; //store last artist
+
+                    foreach (var row in rows)
+                    {
+                        //skip first (header) row
+                        if (count > 0)
+                        {
+                            var columns = row.SelectNodes("td");
+
+                            //column indexes
+                            var colIndexArtist = 0;
+                            var colIndexSong = 1;
+                            var colIndexRating = 2;
+                            var colIndexType = 3;
+
+                            var attemptedBreaking = row.InnerHtml.Contains("THIS APP DOESN'T HAVE RIGHTS TO DISPLAY TABS");
+
+                            if (attemptedBreaking)
+                            {
+                                colIndexArtist += 1;
+                                colIndexSong += 1;
+                                colIndexRating += 1;
+                                colIndexType += 1;
+                            }
+
+                            var rowArtist = columns[colIndexArtist].InnerText;
+
+                            if ((string.IsNullOrEmpty(loopArtist) || loopArtist != rowArtist) && rowArtist != "&nbsp;")
+                            {
+                                loopArtist = HttpUtility.HtmlDecode(rowArtist);
+                            }
+
+                            var rowType = GetTabType(columns[colIndexType].InnerText);
+
+                            if (rowType != null)
+                            {
+                                var rowURL = columns[colIndexSong].ChildNodes["a"].Attributes["href"].Value;
+                                var rowSong = HttpUtility.HtmlDecode(columns[colIndexSong].ChildNodes["a"].InnerText);
+
+                                var rating = TablatureRating.None;
+
+                                var ratingColumn = columns[colIndexRating];
+
+                                if (ratingColumn.InnerText.Contains("["))
+                                {
+                                    var ratingSpan = ratingColumn.ChildNodes["span"].ChildNodes["span"];
+
+                                    if (ratingSpan != null)
+                                    {
+                                        int rowRating;
+                                        if (Int32.TryParse(ratingSpan.Attributes["class"].Value.Replace("r_", ""), out rowRating))
+                                        {
+                                            rating = GetRating(rowRating);
+                                        }
+                                    }
+                                }
+
+                                if (query.Type == null || rowType == query.Type)
+                                {
+                                    var tab = new AttributedTablature(loopArtist, rowSong, rowType);
+                                    results.Add(new SearchResult(query, tab, new Uri(rowURL), rating));
+                                }
+                            }
+                        }
+
+                        count++;
+                    }
+                }
+            }
+
+            return results.ToArray();
+        }
+
+        public bool SupportsTabType(TablatureType type)
+        {
+            return type == TablatureType.Guitar || type == TablatureType.Chords || type == TablatureType.Bass || type == TablatureType.Drum || type == TablatureType.Ukulele;
+        }
+
+        #endregion
+
+        #region Static Methods
+
+        private static TablatureRating GetRating(int value)
+        {
+            switch (value)
+            {
+                case 1:
+                    return TablatureRating.Stars1;
+                case 2:
+                    return TablatureRating.Stars2;
+                case 3:
+                    return TablatureRating.Stars3;
+                case 4:
+                    return TablatureRating.Stars4;
+                case 5:
+                    return TablatureRating.Stars5;
+            }
+
+            return TablatureRating.None;
+        }
+
+        private static TablatureType GetTabType(string str)
+        {
+            if (str.Equals("tab", StringComparison.InvariantCultureIgnoreCase))
+                return TablatureType.Guitar;
+            if (str.Equals("chords", StringComparison.InvariantCultureIgnoreCase))
+                return TablatureType.Chords;
+            if (str.Equals("bass", StringComparison.InvariantCultureIgnoreCase))
+                return TablatureType.Bass;
+            if (str.Equals("drums", StringComparison.InvariantCultureIgnoreCase))
+                return TablatureType.Drum;
+            if (str.Equals("ukulele", StringComparison.InvariantCultureIgnoreCase))
+                return TablatureType.Ukulele;
+
+            return null;
+        }
+
+        #endregion
+    }
+}
